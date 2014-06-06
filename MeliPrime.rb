8e339288b986d2c267ee455b9e770652cd39d936
cutewrath <wonderwoman_is@hotmail.com>
2014-06-05 22:52:55 -0700

    More MeliPrime
    Realized I left out some stuff, so added it, but still not going past
    3....

--- MeliPrime.rb
+++ MeliPrime.rb
@@ -1,9 +1,11 @@
 def is_prime? (number)
 divisor = 2 #start at 2, if start at 1 would result in false negative. Besides, primes are divisible by 1!
-	if number%divisor == 0 
+	if (number == 1 or number == 2)
+		return true
+	elsif number%divisor == 0 
 		return false
 	else
-		return true
+		return true 
 	end while (divisor <number) #ending divisor loop
 divisor = divisor + 1 
 end #ending def
@@ -14,7 +16,7 @@ begin
 	if is_prime? (number) #if is_prime? is true
 		puts number.to_s #then the number number will be listed - should see 1, 2, 3, 5, 7, 11... 
 		number = number + 1 #this keeps the checker moving
-	end while (number <= 10) #ending number loop
+	end while (number <= 1000) #ending number loop
 end #ending begin 
 fin_time = Time.now
 puts (fin_time-start_time)
