82b4430e0a0563987712d20cedb128518d89d738
cutewrath <wonderwoman_is@hotmail.com>
2014-06-05 22:20:54 -0700

    Updated MeliPrime
    Works up to 3 then hangs indefinitely... BUT IT WORKED UP TO 3!!!!!

--- MeliPrime.rb
+++ MeliPrime.rb
@@ -1,23 +1,20 @@
-start_time = Time.now
-x = (1..100) # finding prime numbers 100 and lower
-t = (1..100) #need to divide x by every number x & lower to determine if prime. Keeps barfing when I add while <=x. Prime is only divisible by 1 and self.
-def is_prime? (x)
-	y = (x/t).to_i
-	z = y*t
-	if z=x
+def is_prime? (number)
+divisor = 2 #start at 2, if start at 1 would result in false negative. Besides, primes are divisible by 1!
+	if number%divisor == 0 
 		return false
 	else
 		return true
-	end
-
-x=1 # this will start cycling through the range testing for if prime
+	end while (divisor <number) #ending divisor loop
+divisor = divisor + 1 
+end #ending def
+#==================================================================================
+start_time = Time.now
+number = 1 # this will start cycling through the range testing for if prime
 begin
-	if is_prime? (x) #if is_prime? is true
-		puts x.to_s #then the number x will be listed - should see 1, 2, 3, 5, 7, 11... 
-		x = x+1 #this keeps the checker moving
-	end while (x <=100) # this stops the checker - done at 100
-end
-end
-
+	if is_prime? (number) #if is_prime? is true
+		puts number.to_s #then the number number will be listed - should see 1, 2, 3, 5, 7, 11... 
+		number = number + 1 #this keeps the checker moving
+	end while (number <= 10) #ending number loop
+end #ending begin 
 fin_time = Time.now
 puts (fin_time-start_time)
