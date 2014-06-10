d84e325236af26f3a8391c1127ea7d9a1a3fa46b
cutewrath <wonderwoman_is@hotmail.com>
2014-06-09 22:46:47 -0700

    MeliPrime Again
    I think this one is good!

--- MeliPrime.rb
+++ MeliPrime.rb
@@ -1,23 +1,18 @@
 def is_prime? (x)
-	if (x == 1 or x == 2)
-		return true
-	end
-	y = 2 #start at 2, if start at 1 would result in false negative. Besides, primes are divisible by 1!
-	if x%y == 0 
-		return false
-	elsif (Math.sqrt(x))/(y) == 1
-		return false
-	else 
-		return true 
-	y=y+1 
-	end while (y<x) #ending y loop	
+	return true if (x == 1 or x == 2)
+	y = 2  
+	begin
+		return false if x%y == 0 
+		y=y+1 
+	end while (y<x) #ending begin 
+	return true 
 end #ending def
 
 start_time=Time.now
 x=1
 begin
 	if is_prime?(x)  
-		puts x.to_s #then the x x will be listed - should see 1, 2, 3, 5, 7, 11... 
+		puts x.to_s #then the x number will be listed - should see 1, 2, 3, 5, 7, 11... 
 	end
 	x=x + 1 #this keeps the checker moving
 end while (x<1000) #ending if
